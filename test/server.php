6dc4213183451ef39ff6adf03753a48867f453d1
Alex Guo <chessnut@outlook.com>
2021-05-06 18:25:09 -0500

    gosdk-wasm, test initializeConfig, GetClientEncryptedPublicKey

--- test/server.php
+++ test/server.php
@@ -1,4 +1,12 @@
 <?php
+
+// Fix CORS.
+// header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+
 //router.php
 $path = pathinfo($_SERVER["SCRIPT_FILENAME"]);
 if ($path["extension"] == "wasm") {
